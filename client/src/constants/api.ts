fa531d758518a346f2e26ae7a0093e18811c54b8
KugaChihiro <KugaChihiro@users.noreply.github.com>
2025-04-15 18:00:48 +0900

    ローカル環境の構築2

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,2 +1,2 @@
 // export const apiUrl = "https://conappvr03.thankfulflower-d9ed11c0.eastasia.azurecontainerapps.io";
-export const apiUrl = "http://localhost:8000";
+export const apiUrl = "http://localhost:8000/";
