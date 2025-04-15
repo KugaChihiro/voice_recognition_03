5340a7892f305fe41f4adce3cb43fa37bcecdf82
KugaChihiro <KugaChihiro@users.noreply.github.com>
2025-04-15 14:48:59 +0900

    ファイル構造の見直し2

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,3 +1,2 @@
 export const apiUrl = "https://conappvr03.thankfulflower-d9ed11c0.eastasia.azurecontainerapps.io";
-// export const apiUrl = "https://ca-vr-dev-010.politecoast-4904dd91.eastasia.azurecontainerapps.io";
 
