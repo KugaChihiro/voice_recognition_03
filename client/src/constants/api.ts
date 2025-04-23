802ed896963120583d7c4b3cfb309f73b84b36ab
KugaChihiro <KugaChihiro@users.noreply.github.com>
2025-04-23 14:52:29 +0900

    container app イングレス有効化に伴うURLの変更

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,1 +1,1 @@
-export const apiUrl = "https://conappvr03-test.internal.thankfulflower-d9ed11c0.eastasia.azurecontainerapps.io";
+export const apiUrl = "https://conappvr03-test.thankfulflower-d9ed11c0.eastasia.azurecontainerapps.io";
